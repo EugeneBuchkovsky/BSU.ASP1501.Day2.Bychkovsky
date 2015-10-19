346a5e7ea981876c536cab523b5f5b98408b8e57
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-19 23:01:58 +0200

    Task4

---
+++ BSU.ASP1501.Day2.Bychkovsky.Task4.Test/LogicEuclidMethodTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using BSU.ASP1501.Day2.Bychkovsky.Task4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BSU.ASP1501.Day2.Bychkovsky.Task4.Test
+{
+    [TestClass]
+    public class LogicEuclidMethodTest
+    {
+        [TestMethod]
+        public void GCD_35and15_5()
+        {
+            int ExpectedResult = 5;
+
+            Assert.AreEqual(ExpectedResult, LogicEuclidMethod.GCD(35, 15));
+        }
+
+        [TestMethod]
+        public void GCD_36and15and27_3()
+        {
+            int ExpectedResult = 3;
+
+            Assert.AreEqual(ExpectedResult, LogicEuclidMethod.GCD(36, 15, 27));
+        }
+
+        [TestMethod]
+        public void GCD_36and155and33and345and12_1()
+        {
+            int ExpectedResult = 1;
+
+            Assert.AreEqual(ExpectedResult, LogicEuclidMethod.GCD(36, 155, 33, 345, 12));
+        }
+
+        [TestMethod]
+        public void BinaryAlgorithm_45and15_15()
+        {
+            int ExpectedResult = 15;
+
+            Assert.AreEqual(ExpectedResult, LogicEuclidMethod.BinaryAlgorithm(45, 15));
+        }
+
+        [TestMethod]
+        public void BinaryAlgorithm_36and15and27_3()
+        {
+            int ExpectedResult = 3;
+
+            Assert.AreEqual(ExpectedResult, LogicEuclidMethod.BinaryAlgorithm(36, 15, 27));
+        }
+
+        [TestMethod]
+        public void BinaryAlgorithm_36and15and27and414and3000_3()
+        {
+            int ExpectedResult = 3;
+
+            Assert.AreEqual(ExpectedResult, LogicEuclidMethod.BinaryAlgorithm(36, 15, 27, 414, 3000));
+        }
+    }
+}
---
+++ BSU.EPAM.1501.Day2.Bychkovsky/LogicEuclidMethod.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace BSU.ASP1501.Day2.Bychkovsky.Task4
+{
+    public static class LogicEuclidMethod
+    {
+        /// <summary>
+        /// Euclid's algorithm for 2 arguments.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int GCD(int m, int n)
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            while (n != 0)
+                n = m % (m = n);
+            time.Stop();
+            return Math.Abs(m);
+        }
+
+        /// <summary>
+        /// Euclid's algorithm for 3 arguments.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int GCD(int m, int n, int k)
+        {
+            return GCD(GCD(m,n), k);
+        }
+
+        /// <summary>
+        /// Euclid's algorithm for n arguments.
+        /// </summary>
+        /// <param name="arrayInt"></param>
+        /// <returns></returns>
+        public static int GCD(params int[] arrayInt)
+        {
+            int fTemp = 0, temp = GCD(arrayInt[0],arrayInt[1]);
+            for (int i = 1; i < arrayInt.Length - 1; i++)
+            {
+                if (temp == 1)
+                    return temp;
+                fTemp = GCD(temp, arrayInt[i]);
+                temp = fTemp;
+            }
+            return temp;
+        }
+
+        /// <summary>
+        /// Binary Euclid's algorithm for 2 arguments.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int BinaryAlgorithm(int m, int n)
+        {
+            if (m == n)
+                return m;
+            if (m == 0)
+                return m;
+            if (n == 0)
+                return n;
+            if (m == 1 || n == 1)
+                return 1;
+            if ((m % 2 == 0) && (n % 2 == 0))
+                return 2 * BinaryAlgorithm(m / 2, n / 2);
+            if ((m % 2 == 0) && (n % 2 == 1))
+                return BinaryAlgorithm(m / 2, n);
+            if ((m % 2 == 1) && (n % 2 == 0))
+                return BinaryAlgorithm(m, n / 2);
+            return BinaryAlgorithm(n, Math.Abs(m - n));
+        }
+
+        /// <summary>
+        /// Binary Euclid's algorithm for 3 arguments.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int BinaryAlgorithm(int m, int n, int k)
+        {
+            return BinaryAlgorithm(BinaryAlgorithm(m, n), k);
+        }
+
+        /// <summary>
+        /// Binary Euclid's algorithm for n arguments.
+        /// </summary>
+        /// <param name="arrayInt"></param>
+        /// <returns></returns>
+        public static int BinaryAlgorithm(params int[] arrayInt)
+        {
+            int fTemp = 0, temp = BinaryAlgorithm(arrayInt[0], arrayInt[1]);
+            for (int i = 1; i < arrayInt.Length - 1; i++)
+            {
+                if (temp == 1)
+                    return temp;
+                fTemp = BinaryAlgorithm(temp, arrayInt[i]);
+                temp = fTemp;
+            }
+            return temp;
+        }
+    }
+}
