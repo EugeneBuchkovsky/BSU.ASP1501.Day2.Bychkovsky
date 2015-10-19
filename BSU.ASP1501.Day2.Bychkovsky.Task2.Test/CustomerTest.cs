a8a6ebd4f78b96c63135fffe07f7fd1679a5eb76
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-19 18:51:29 +0200

    Task2

---
+++ BSU.ASP1501.Day2.Bychkovsky.Task2.Test/CustomerTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BSU.ASP1501.Day2.Bychkovsky.Task2;
+
+namespace BSU.ASP1501.Day2.Bychkovsky.Task2.Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void ToString_withoutArg_allProperties()
+        {
+            string name = "Eugene";
+            string contactPhone = "+ 375 29 887 16 58";
+            decimal revenue = 7500000000;
+            string ExpectedResult = String.Format("Customer name: {0}\nContact phone: {1}\nRevenue: {2:C}", name, contactPhone, revenue);
+              
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString());
+        }
+
+        [TestMethod]
+        public void ToString_withArgN_Name()
+        {
+            string name = "Eugene";
+
+            string ExpectedResult = String.Format("Customer name: {0}", name);
+
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString("N"));
+        }
+
+        [TestMethod]
+        public void ToString_withArgP_Phone()
+        {
+            string contactPhone = "+ 375 29 887 16 58";
+            string ExpectedResult = String.Format("Contact phone: "+ contactPhone);
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString("P"));
+        }
+
+        [TestMethod]
+        public void ToString_withArgR_Revenue()
+        {
+            decimal rev = 7500000000;
+            string ExpectedResult = String.Format("Revenue: {0:C}",rev);
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString("R"));
+        }
+
+        [TestMethod]
+        public void ToString_withArgNP_NamePhone()
+        {
+            string name = "Eugene";
+            string contactPhone = "+ 375 29 887 16 58";
+            string ExpectedResult = String.Format("Customer name: {0}\nContact phone: {1}", name, contactPhone);
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString("NP"));
+        }
+
+        [TestMethod]
+        public void ToString_withArgNR_NameRev()
+        {
+            string name = "Eugene";
+            decimal revenue = 7500000000;
+            string ExpectedResult = String.Format("Customer name: {0}\nRevenue: {1:C}", name, revenue);
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString("NR"));
+        }
+
+        [TestMethod]
+        public void ToString_withArgPR_PhoneRev()
+        {
+            string contactPhone = "+ 375 29 887 16 58";
+            decimal revenue = 7500000000;
+            string ExpectedResult = String.Format("Contact phone: {0}\nRevenue: {1:C}", contactPhone, revenue);
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString("PR"));
+        }
+
+        [TestMethod]
+        public void ToString_withUserProvider_allPropertiesLocalUk()
+        {
+            string name = "Eugene";
+            string contactPhone = "+ 375 29 887 16 58";
+            decimal revenue = 7500000000;
+            CultureInfo uk = new CultureInfo("en-US");
+            string ExpectedResult = String.Format(uk, "Customer name: {0}\nContact phone: {1}\nRevenue: {2:C}", name, contactPhone, revenue);
+
+            Customer user = new Customer("Eugene", "+ 375 29 887 16 58", 7500000000);
+
+            Assert.AreEqual(ExpectedResult, user.ToString(uk));
+        }
+    }
+}
---
+++ BSU.EPAM.1501.Day2.Bychkovsky.Task2/Customer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSU.ASP1501.Day2.Bychkovsky.Task2
+{
+    public class Customer : IFormattable
+    {
+        public string Name { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public decimal Revenue { get; set; }
+
+        public Customer(string name, string contactPhone, decimal rev)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = rev;
+        }
+
+        /// <summary>
+        /// String representation of the object Customer.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return String.Format(CultureInfo.CurrentCulture, "Customer name: {0}\nContact phone: {1}\nRevenue: {2:C}", Name, ContactPhone, Revenue);
+        }
+
+        /// <summary>
+        /// String representation of the object Customer with user's IFormatProvider
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public string ToString(IFormatProvider provider)
+        {
+            return String.Format(provider, "Customer name: {0}\nContact phone: {1}\nRevenue: {2:C}", Name, ContactPhone, Revenue);
+        }
+
+        /// <summary>
+        /// String representation the part of data of the object Customer.
+        /// </summary>
+        /// <param name="Format"></param>
+        /// <returns></returns>
+        public string ToString(string Format)
+        {
+            if (string.IsNullOrEmpty(Format))
+                return ToString();
+            switch (Format.ToUpperInvariant())
+            {
+                case "NP": return String.Format(CultureInfo.CurrentCulture, "Customer name: {0}\nContact phone: {1}", Name, ContactPhone);
+                case "NR": return String.Format(CultureInfo.CurrentCulture, "Customer name: {0}\nRevenue: {1:C}", Name, Revenue);
+                case "PR": return String.Format(CultureInfo.CurrentCulture, "Contact phone: {0}\nRevenue: {1:C}", ContactPhone, Revenue);
+                case "N": return String.Format(CultureInfo.CurrentCulture, "Customer name: {0}", Name);
+                case "P": return String.Format(CultureInfo.CurrentCulture, "Contact phone: {0}", ContactPhone);
+                case "R": return String.Format(CultureInfo.CurrentCulture, "Revenue: {0:C}", Revenue);
+                default: throw new Exception("Wrong format!");
+            }
+        }
+
+        /// <summary>
+        /// String representation the part of data of the object Customer with user's IFormatProvider
+        /// </summary>
+        /// <param name="Format"></param>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public string ToString(string Format, IFormatProvider provider)
+        {
+            if (string.IsNullOrEmpty(Format))
+                return ToString();
+            if (provider == null) 
+                provider = CultureInfo.CurrentCulture;
+            switch (Format.ToUpperInvariant())
+            {
+                case "NP": return String.Format(provider, "Customer name {0}\nContact phone {1}", Name, ContactPhone);
+                case "NR": return String.Format(provider, "Customer name {0}\nRevenue {1:C}", Name, Revenue);
+                case "PR": return String.Format(provider, "Contact phone {0}\nRevenue {1:C}", ContactPhone, Revenue);
+                case "N": return String.Format(provider, "Customer name {0}", Name);
+                case "P": return String.Format(provider, "Contact phone {0}",ContactPhone);
+                case "R": return String.Format(provider, "Revenue {0:C}",Revenue);
+                default: throw new Exception("Wrong format!");
+            }
+        }
+    }
+}
