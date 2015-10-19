7282215b2190771e8654b1f6ed46764d6b26893e
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-19 18:54:33 +0200

    Task3

---
+++ BSU.ASP1501.Day2.Bychkovsky.Task3.Test/ToHexadecimalFormatProviderTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BSU.ASP1501.Day2.Bychkovsky.Task3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BSU.ASP1501.Day2.Bychkovsky.Task3.Test
+{
+    [TestClass]
+    public class ToHexadecimalFormatProviderTest
+    {
+        [TestMethod]
+        public void Format_100inHesSystem_64()
+        {
+            string ExpectedString = String.Format("{0:X}", 100);
+
+            IFormatProvider fp = new ToHexadecimalFormatProvider();
+            string result = String.Format(fp, "{0:H}", 100);
+
+            Assert.AreEqual(ExpectedString, result);
+        }
+    
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Format_nullArgument_ArgumentNullException()
+        {
+            Exception error = new ArgumentNullException();
+
+            IFormatProvider fp = new ToHexadecimalFormatProvider();
+            string result = String.Format(fp, "{0:H}", null);
+
+            Assert.AreEqual(error, result);
+        }
+    }
+}
---
+++ BSU.EPAM.1501.Day2.Bychkovsky.Task3/ToHexadecimalFormatProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSU.ASP1501.Day2.Bychkovsky.Task3
+{
+    /// <summary>
+    /// Class for representation integer in hexadecimal.
+    /// </summary>
+    public class ToHexadecimalFormatProvider : IFormatProvider, ICustomFormatter
+    {
+            static readonly string Hnumbers = "0123456789ABCDEF";
+
+            IFormatProvider _parent;   // Allows consumers to chain format providers
+
+            public ToHexadecimalFormatProvider() : this(CultureInfo.CurrentCulture) { }
+
+            public ToHexadecimalFormatProvider(IFormatProvider parent)
+            {
+                _parent = parent;
+            }
+
+            public object GetFormat(Type formatType)
+            {
+                if (formatType == typeof(ICustomFormatter)) return this;
+                return null;
+            }
+
+            public string Format(string format, object arg, IFormatProvider prov)
+            {
+                if ((arg == null))
+                    throw new ArgumentNullException();
+
+                if (format != "H" || (arg.GetType() != typeof(int)))
+                    return string.Format(_parent, "{0:" + format + "}", arg);
+
+                StringBuilder result = new StringBuilder();
+                string digitList = string.Format(CultureInfo.InvariantCulture, "{0}", arg);
+
+                int res = (int)arg, residue;
+                while (res != 0)
+                {
+                    residue = res % 16;
+                    result.Append(Hnumbers[residue]);
+                    res /= 16;
+                }
+
+                char[] reverseResult = result.ToString().ToCharArray();
+                Array.Reverse(reverseResult);
+                return new string(reverseResult);
+            }
+        
+    }
+}
